90e50390f5b73539da8bcb44d98f111e0b824ec7
Austin P <auspatterson911@gmail.com>
2024-01-11 06:59:42 +0000

    01_Trebuchet complete

---
+++ 01_Trebuchet.js
@@ -0,0 +1,61 @@
+/******************
+ * Day 1: Trebuchet?!
+ * https://adventofcode.com/2023/day/1
+ * Final answer: 55002
+ * 
+ * Part 2: digits can be spelled out
+ * https://adventofcode.com/2023/day/1#part2
+ * Final answer: ?
+ */
+
+// Part 1 solution
+
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: fs.createReadStream('01_input.txt'),
+});
+
+let globalSum = 0;
+
+function parse(line) {
+  let a = NaN, b = NaN;
+  let cursor = 0;
+
+  while (cursor < line.length && isNaN(a)) {
+    a = Number(line.charAt(cursor));
+    cursor++;
+  }
+
+  cursor = line.length - 1;
+  while (cursor >= 0 && isNaN(b)) {
+    b = Number(line.charAt(cursor));
+    cursor--;
+  }
+
+  if (!isNaN(a) && !isNaN(b)) {
+    globalSum += (a * 10 + b);
+  }
+}
+
+function printSum(a, b) {
+  console.log(a + b);
+}
+
+// RUN
+
+rl.on('line', (line) => {
+  if (line.trim() === '') {
+    rl.close();
+  } else {
+    parse(line);
+    rl.prompt();
+  }
+});
+
+rl.on('close', () => {
+  console.log(globalSum);
+});
+
+rl.prompt();
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AdventOfCode2023
+
+Everything is Part 1 unless otherwise named.
+
 https://adventofcode.com/2023/day/1
